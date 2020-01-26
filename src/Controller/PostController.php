ccaffa6b1f0c66b7bb37935b38a66593a5a9b587
saitama93 <igalilmi32@gmail.com>
2020-01-26 17:53:49 +0000

    Entié liké mise en place et de la method like

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,13 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Repository\PostLikeRepository;
 use App\Repository\PostRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
     /**
+     * Permet d'afficher la liste des articles
+     * 
+     * @param PostRepository $repo
+     * @return Response
+     * 
      * @Route("/", name="homepage")
      */
     public function index(PostRepository $repo)
@@ -16,5 +25,20 @@ class PostController extends AbstractController
         return $this->render('post/index.html.twig', [
             'posts' => $repo->findAll(),
         ]);
+    }
+
+    /**
+     * Permet de liker ou unliker un article
+     *
+     * @param Post $post
+     * @param ObjectManager $manager
+     * @param PostLikeRepository $likeRepo
+     * @return Response
+     * 
+     * @Route("/post/{id}/like", name="post_like")
+     */
+    public function like(Post $post, ObjectManager $manager, PostLikeRepository $likeRepo): Response
+    {
+        return $this->json(['code' => 200, 'message' => 'Ca marche !'], 200);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Post;
+use App\Entity\PostLike;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -27,19 +28,45 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create();
 
+        $users = [];
+
         $user = new User();
         $user->setEmail('user@symfony.com')
             ->setPassword($this->encoder->encodePassword($user, 'password'));
 
         $manager->persist($user);
 
+        $users[] = $user;
+
         for ($i = 0; $i < 20; $i++) {
+
+            $user = new User();
+            $user->setEmail($faker->email())
+                ->setPassword($this->encoder->encodePassword($user, 'password'));
+
+            $manager->persist($user);
+
+            $users[] = $user;
+        }
+
+        for ($j = 0; $j < 20; $j++) {
+
             $post = new Post();
             $post->setTitle($faker->sentence(6))
                 ->setIntroduction($faker->paragraph())
                 ->setContent('<p>' . join(',', $faker->paragraphs()) . '</p>');
 
             $manager->persist($post);
+
+            for ($z = 0; $z < mt_rand(0, 10); $z++) {
+
+                $like = new PostLike();
+
+                $like->setPost($post)
+                    ->setUser($faker->randomElement($users));
+
+                $manager->persist($like);
+            }
         }
 
         $manager->flush();
